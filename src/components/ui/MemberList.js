3d60e05c65ac82753b337d62bb017d350b5c3c9c
Oludolapo Soboyede <dolimoon6000@yahoo.com>
2017-06-02 22:49:25 +0100

    Flesh out and display MemberList

--- src/components/ui/MemberList.js
+++ src/components/ui/MemberList.js
@@ -1,11 +1,38 @@
 import { Component } from 'react'
 import fetch from 'isomorphic-fetch'
+import Member from './Member'
 
 class MemberList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            members: [
+            {
+                name: "Joe Wilson",
+                email: "joe.wilson@example.com",
+                thumbnail: "https://randomuser.me/api/portraits/men/53.jpg"
+            },
+            {
+                name: "Stacy Gardner",
+                email: "stacy.gardner@example.com",
+                thumbnail: "https://randomuser.me/api/portraits/women/74.jpg"
+            },
+            {
+                name: "Billy Young",
+                email: "billy.young@example.com",
+                thumbnail: "https://randomuser.me/api/portraits/men/34.jpg"
+            }
+          ]
+        }
+    }
+
     render() {
         return (
             <div className="member-list">
             	<h1>Society Members</h1>
+                {this.state.members.map((member, i) =>
+                    <Member key={i} {...member} makeAdmin={email => console.log(email) } />
+                )}
             </div>
         )    
    }     
